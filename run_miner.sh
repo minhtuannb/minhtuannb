03ae36896a6ba6e7a315a443f11a22183b35c186
minhtuannb <101493977+minhtuannb@users.noreply.github.com>
2023-04-06 22:49:01 +0700

    Delete run_miner.sh

--- run_miner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# For more usage info: hellminer --help
-
-# Example SSL URL
-# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
-
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RE8cjrMCuqDypDkVvv3rZZGmwkgSAENi1Z.may2 -p x --cpu 4
